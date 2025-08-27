7be51702f0a41bcdbdc1179e4f18cc8e0ec7bb26
Luis <luigimonge16@gmail.com>
2025-08-27 10:46:20 +0200

    Add comments

--- src/training.py
+++ src/training.py
@@ -3,6 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 def fit_model(X_train, y_train):
     """Train a RandomForestClassifier model."""
+    # Proposed modification: allow for receiving the model as a parameter so you can generalize the function to fit any model.
     clf = RandomForestClassifier()
     clf.fit(X_train, y_train)
     return clf
